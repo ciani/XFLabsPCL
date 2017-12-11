881b7b4a8a6c331718ce60b56797d1b871db1070
Ciani Afonso Díaz <ciani@devsdna.com>
2017-12-12 00:12:30 +0100

    Connected to Azure Notification Hub

---
+++ XFLabsPCL/XFLabsPCL.Android/Services/FirebaseService/FirebaseNotificationService.cs
@@ -0,0 +1,38 @@
+﻿namespace XFLabsPCL.Droid.Services
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Util;
+    using Firebase.Messaging;
+
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseNotificationService : FirebaseMessagingService
+    {
+        const string TAG = "FirebaseNotificationService";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification message body: " + message);
+            ShowNotification(message.GetNotification().Body);
+        }
+
+        void ShowNotification(string messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new Notification.Builder(this)
+                .SetSmallIcon(Resource.Drawable.notification_template_icon_bg)
+                .SetContentTitle("FCM Message")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
---
+++ XFLabsPCL/XFLabsPCL.Android/Services/FirebaseService/FirebaseRegistrationService.cs
@@ -0,0 +1,55 @@
+﻿namespace XFLabsPCL.Droid.Services
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Util;
+    using Firebase.Iid;
+    using System;
+    using System.Diagnostics;
+    using WindowsAzure.Messaging;
+
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class FirebaseRegistrationService : FirebaseInstanceIdService
+    {
+        private NotificationHub Hub;
+        const string TAG = "FirebaseRegistrationService";
+
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            SendRegistrationTokenToAzureNotificationHub(refreshedToken);
+        }
+
+        private void SendRegistrationTokenToAzureNotificationHub(string token)
+        {
+            // Register notification hub registration
+            Subscribe("AccionaNH"
+                    , "Endpoint=sb://accionans.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=a8qdFRh2rdlrLDsbXa3xfeb/SkoUSjB4VBdWKgXbNrA="
+                    , token);
+        }
+
+        public void Subscribe(string hubNotificationPath, string connectionString, string token)
+        {
+            Hub = new NotificationHub(hubNotificationPath, connectionString, Application.Context);
+
+            try
+            {
+                Hub.UnregisterAll(token);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            try
+            {
+                var hubRegistration = Hub.Register(token);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
