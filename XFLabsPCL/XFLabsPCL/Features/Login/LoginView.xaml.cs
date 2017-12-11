6c49317d1c98d48d0ff944edc2362c3125e37972
Ciani Afonso Díaz <ciani@devsdna.com>
2017-12-11 20:52:20 +0100

    Add project files.

---
+++ XFLabsPCL/XFLabsPCL/Features/Login/LoginView.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace XFLabsPCL.Features.Login
+{
+    using System;
+    using ReactiveUI;
+    using Xamarin.Forms.Xaml;
+    using Xamarin.Forms;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+            BindingContext = new LoginViewModel();
+            this.WhenActivated(d =>
+            {
+                d(this.Bind(ViewModel, vm => vm.User, v => v.EntryUser.Text));
+                d(this.Bind(ViewModel, vm => vm.Pass, v => v.EntryPass.Text));
+
+                d(this.BindCommand(ViewModel, vm => vm.DoLogin, v => v.BtnLogin));
+                d(this.Bind(ViewModel, vm => vm.IsLoading, v => v.ActIsLoading.IsVisible));
+
+                d(this.WhenAny(v => v.EntryPass.Text, x => x.Value)
+                  .Subscribe(pass =>
+                  {
+                      if (!string.IsNullOrWhiteSpace(pass))
+                          EntryPass.BackgroundColor = Color.Green;
+                      else
+                          EntryPass.BackgroundColor = Color.White;
+
+                  }));
+
+
+            });
+        }
+    }
+}
---
+++ XFLabsPCL/XFLabsPCL/Features/Login/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿namespace XFLabsPCL.Features.Login
+{
+    using ReactiveUI;
+    using System;
+    using System.Threading.Tasks;
+
+    public class LoginViewModel : ReactiveObject
+    {
+        private string user;
+        private string pass;
+        private ReactiveCommand doLoginAsync;
+        private ObservableAsPropertyHelper<bool> isLoading;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoginViewModel"/> class.
+        /// </summary>
+        public LoginViewModel()
+        {
+            doLoginAsync = ReactiveCommand.CreateFromTask(DoLoginAsync, CanExecuteLogin());
+            doLoginAsync.IsExecuting.ToProperty(this, vm => vm.IsLoading, out isLoading);
+
+        }
+
+        public string User
+        {
+            get => user;
+            set => this.RaiseAndSetIfChanged(ref user, value);
+        }
+
+        public string Pass
+        {
+            get => pass;
+            set => this.RaiseAndSetIfChanged(ref pass, value);
+        }
+
+        public bool IsLoading => isLoading?.Value ?? false;
+
+
+        public ReactiveCommand DoLogin => doLoginAsync;
+
+        private async Task DoLoginAsync()
+        {
+            await Task.Delay(2000);
+        }
+
+
+        private IObservable<bool> CanExecuteLogin()
+        {
+            return this.WhenAnyValue(vm => vm.User, vm => vm.Pass, vm => vm.IsLoading, (usr, pwd, ldg) =>
+            {
+                return !ldg &&
+                !string.IsNullOrWhiteSpace(usr) &&
+                !string.IsNullOrWhiteSpace(pwd);
+            });
+        }
+
+    }
+}
